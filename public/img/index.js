23ffa12d693d25a92fb7d5f403c7724a78301a75
Foparty <alfonsomartingarcia@gmail.com>
2023-05-23 10:39:48 +0200

    landing features done

--- public/img/index.js
+++ public/img/index.js
@@ -2,3 +2,7 @@ export { default as logo } from './netflixlogo.svg';
 export { default as avatar } from './comicavatar.png';
 export { default as tmdb } from './tmdb.svg';
 export { default as landingcover } from './landingcover.webp';
+export { default as landing1 } from './landing1.png';
+export { default as landing2 } from './landing2.png';
+export { default as landing3 } from './landing3.png';
+export { default as landing4 } from './landing4.png';
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
 import styles from './Landing.module.css';
-import { landingcover, logo } from '../../../public/img/index.js';
+import {
+	landing1,
+	landing2,
+	landing3,
+	landing4,
+	landingcover,
+	logo,
+} from '../../../public/img/index.js';
 import { TbWorld } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
+
+const features = [
+	{
+		id: 1,
+		title: 'Watch on TV',
+		sub: 'Watch on Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray and more.',
+		img: landing1,
+	},
+	{
+		id: 2,
+		title: 'Watch everywhere',
+		sub: 'Watch movies, series and programs without limits on your phone, tablet, laptop and TV.',
+		img: landing2,
+	},
+	{
+		id: 3,
+		title: 'Create profiles for children',
+		sub: "Let your kids go on adventures with their favorite characters in a special section of the site that's free with membership.",
+		img: landing3,
+	},
+	{
+		id: 4,
+		title: 'Download your favorite titles and watch offline',
+		sub: 'Save your favorite titles so you always have something to watch.',
+		img: landing4,
+	},
+];
 
 export const Landing = () => {
 	return (
@@ -21,7 +54,9 @@ export const Landing = () => {
 							<option value="en">English</option>
 						</select>
 					</label>
-					<button>Sign In</button>
+					<button>
+						<Link to={'/home'}>Sign In</Link>
+					</button>
 				</div>
 			</header>
 			<main className={styles.content}>
@@ -30,13 +65,13 @@ export const Landing = () => {
 						<img src={landingcover} alt="netflix background" />
 						<div className={styles.overlay}></div>
 						<div className={styles.started}>
-							<h1>Unlimited movies, TV shows, and more</h1>
+							<h1>This is just a demo, click on any red button to access</h1>
 							<h2>Watch anywhere. Cancel anytime.</h2>
 							<p>
 								Ready to watch? Enter your email to create or restart your
 								membership.
 							</p>
-							<form>
+							<form className={styles.form}>
 								<input type="text" placeholder={'does not wotk so skip it'} />
 								<Link className={styles.button} to={'/home'}>
 									Enter the DEMO
@@ -44,6 +79,19 @@ export const Landing = () => {
 							</form>
 						</div>
 					</div>
+				</section>
+				<section className={styles.features}>
+					{features.map((item) => (
+						<article key={item.id}>
+							<div className={styles.textcontainer}>
+								<h2 className={styles.title}>{item.title}</h2>
+								<p className={styles.sectiontext}>{item.sub}</p>
+							</div>
+							<div className={styles.imgcontainer}>
+								<img src={item.img} alt="" />
+							</div>
+						</article>
+					))}
 				</section>
 			</main>
 		</>
