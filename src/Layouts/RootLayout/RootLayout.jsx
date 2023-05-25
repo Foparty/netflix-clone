55eaabf31aa3b770387e80c34e64db17ac9abffa
Foparty <alfonsomartingarcia@gmail.com>
2023-05-25 09:05:53 +0200

    sidebar.
    can improve but ok

--- src/Layouts/RootLayout/RootLayout.jsx
+++ src/Layouts/RootLayout/RootLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../../components/Navbar/Navbar.jsx';
 import styles from './RootLayout.module.css';
+import { SideNav } from '../../components/SideNav/SideNav.jsx';
 
 export const RootLayout = () => {
 	return (
-		<header>
-			<Navbar />
+		<>
+			{/*<Navbar />*/}
+			<SideNav />
 			<main className={styles.main}>
 				<Outlet />
 			</main>
-		</header>
+		</>
 	);
 };
---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,38 @@
+import styles from './SideNav.module.css';
+import { BiHomeAlt2, BiMovie } from 'react-icons/bi';
+import { RxMagnifyingGlass } from 'react-icons/rx';
+import { AiOutlineCalendar, AiOutlinePlus } from 'react-icons/ai';
+import { BsDisplay } from 'react-icons/bs';
+
+export const SideNav = () => {
+	return (
+		<aside className={styles.sidebar}>
+			<nav className={styles.nav}>
+				<div className={styles.menugroup}>
+					<RxMagnifyingGlass />
+					<h2 className={styles.menutitle}>Search</h2>
+				</div>
+				<div className={styles.menugroup}>
+					<BiHomeAlt2 />
+					<h2 className={styles.menutitle}>Home</h2>
+				</div>
+				<div className={styles.menugroup}>
+					<AiOutlineCalendar />
+					<h2 className={styles.menutitle}>Latest</h2>
+				</div>
+				<div className={styles.menugroup}>
+					<BsDisplay />
+					<h2 className={styles.menutitle}>TV Shows</h2>
+				</div>
+				<div className={styles.menugroup}>
+					<BiMovie />
+					<h2 className={styles.menutitle}>Movies</h2>
+				</div>
+				<div className={styles.menugroup}>
+					<AiOutlinePlus />
+					<h2 className={styles.menutitle}>Last added</h2>
+				</div>
+			</nav>
+		</aside>
+	);
+};
